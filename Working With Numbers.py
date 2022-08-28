10213282fcb86b24b8cc18fc0a73e9fdf72a967d
GooMoonRyoung <Andrew.truong25@gmail.com>
2022-08-27 22:46:53 -0400

    2nd day of python Coding

---
+++ Working With Numbers.py
@@ -0,0 +1,16 @@
+from math import *
+
+pee = -5
+print(-2.0987)
+print(3 + 4.5)
+print(3 * 4 + 5)
+print(3 * (4 + 5))
+print(abs(pee))
+print(pow(3, 3))
+print(max(2,10))
+print(min(2,10))
+print(round(3.675))
+print(round(7.443))
+print(floor(7.443))
+print(ceil(7.443))
+print(sqrt(36))
---
+++ Working With Strings.py
@@ -0,0 +1,10 @@
+pee = "Andrew Truong"
+
+print(pee.lower())
+print(pee.upper())
+print(pee.isupper())
+print(pee.upper().isupper())
+print(len(pee))
+print(pee[0])
+print(pee.index("T"))
+print(pee.replace("Truong", "Smith"))
