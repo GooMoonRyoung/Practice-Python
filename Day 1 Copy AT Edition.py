d3ab93e38df9983c82f6187f86026b567c0b7776
GooMoonRyoung <Andrew.truong25@gmail.com>
2022-08-27 22:54:05 -0400

    Copying Emersons code

---
+++ Day 1 Copy AT Edition.py
@@ -0,0 +1,5 @@
+print("Wondering what your band name could be? I am here to help you!")
+city = input("What's the name of the city you grew up in?")
+name = input("What's your pet's name?")
+b_name = city + name
+print("Congrats! Your band name could be " + b_name + ", let me know if you like it! :)")
