5a6eb7a2f9c5f0fa7e22492c4750cdb10e5270dc
GooMoonRyoung <Andrew.truong25@gmail.com>
2022-08-29 12:25:35 -0400

    better formatting for the money

--- Day2PythonCopy.py
+++ Day2PythonCopy.py
@@ -1,7 +1,7 @@
 print("Welcome to the tip calculator.")
-bill = float(input("What was th total bill? "))
+bill = float(input("What was the total bill? "))
 #tipp means tip percentage 
 tipp = int(input("What percentage tip would you like to give? 10, 12, or 15? ")) / 100 + 1 
 people = float(input("How many people to split the bill? "))
-ind_bill = round(bill * tipp / people, 2)
+ind_bill = "{:.2f}".format(bill * tipp / people, 2)
 print(f"Each person should pay: ${ind_bill}")
